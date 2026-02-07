dfc145137ed18fb0d1a67f7300abd96abc0dd531
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Air.local>
2026-02-07 12:48:26 +0530

    02: authentication

--- codexia/src/app/layout.tsx
+++ codexia/src/app/layout.tsx
@@ -1,15 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Mono, Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
+
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const plexMono = IBM_Plex_Mono({
+  variable: "--font-plex-mono",
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -23,12 +36,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark
+    }}>
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${plexMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header>
+          <SignedOut>
+            <SignInButton/>
+          <SignUpButton>
+            <button className="bg-rose-500 text-white p-2 rounded">
+              Sign Up
+            </button>
+          </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+          </header>
+          {children}
+          </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
